1317dafb26cce0f698080192cb7a01419ebf1ddb
al1211 <anil.kumar0372915@gmail.com>
2026-02-15 12:06:30 +0530

    add basic setup frontend and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import './index.css'
+import { createBrowserRouter,RouterProvider } from "react-router";
+import Home from "../pages/Home"
+import Login from "../pages/Login"
+import ProductDetails from "../pages/ProductDetails"
+import Signup from "../pages/Singup"
 
-function App() {
 
-  return (
-    <>
-    <h1 className='text-5xl font-bold text- text-red-500'>hello</h1>
-    </>
-  )
+const router=createBrowserRouter([
+  {path:"/",element:<Home/>},
+  {path:"/login",element:<Login/>},
+  {path:"/signup",element:<Signup/>},
+  {path:"/products",element:<ProductDetails/>},
+])
+
+export default function App(){
+  return <RouterProvider router={router}/>
 }
-
-export default App
